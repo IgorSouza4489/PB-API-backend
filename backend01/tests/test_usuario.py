218bdc840accab60edc085c389e8f8af8fcd5e8a
Vitor Ferreira Lopes <100311898+vitorflopes@users.noreply.github.com>
2024-02-26 20:19:22 -0300

    Vaiiiiiiiii

--- backend01/tests/test_usuario.py
+++ backend01/tests/test_usuario.py
@@ -7,13 +7,8 @@ HTTP_BAD_REQUEST = 400
 HTTP_UNAUTHORIZED = 401
 HTTP_SERVER_ERROR = 500
 
+client = app.test_client()
 
 def test_obter_usuarios():
-    # Crie um cliente de teste
-    client = app.test_client()
-    
-    # Faça uma solicitação GET à rota /usuarios
     resposta = client.get('/usuarios')
-    
-    # Verifique se a resposta tem o código de status HTTP_OK
     assert resposta.status_code == HTTP_OK
