4bc6ab47b94ef725e7f552e0c61692d9a374bd9d
Vitor Ferreira Lopes <100311898+vitorflopes@users.noreply.github.com>
2024-02-26 20:39:00 -0300

    2 tests nno back

--- backend01/tests/test_usuario.py
+++ backend01/tests/test_usuario.py
@@ -12,3 +12,17 @@ client = app.test_client()
 def test_obter_usuarios():
     resposta = client.get('/usuarios')
     assert resposta.status_code == HTTP_OK
+
+def test_icluir_usuarios():
+    dados_usuario = {
+        'nome': 'Teste Teste1',
+        'email': 'teste1@example.com',
+        'data_nascimento': '01/01/1990',
+        'senha': 'teste123'
+    }
+
+    resposta = client.post('/usuarios', json=dados_usuario)
+    assert resposta.status_code == HTTP_CREATED
+
+    resposta_json = resposta.get_json()
+    assert resposta_json['mensagem'] == 'Usuário incluído com sucesso!'
